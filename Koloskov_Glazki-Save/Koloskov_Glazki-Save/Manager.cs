794a1b6bfc896a3a8a7c055e03fcf97c9ceac844
ldgen <leondmikol@mail.ru>
2023-11-09 10:44:05 +0500

    laba 10

---
+++ Koloskov_Glazki-Save/Koloskov_Glazki-Save/Manager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Koloskov_Glazki_Save
+{
+    class Manager
+    {
+        public static Frame MainFrame { get; set; }
+    }
+}
